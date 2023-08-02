0bec970b39df22b5466da15082304e10102af26a
repiovezan <repiovezan@hotmail.com>
2023-08-02 08:51:18 -0300

    aula 2

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
+
+@Controller("auth")
+    export class AuthController {
+        constructor(private readonly service: AuthService) { }
+
+        @Post('login')
+    @HttpCode(HttpStatus.OK)
+    login(@Body() dto: LoginDto) {
+
+
+        return this.service.login(dto);
+}
+}
